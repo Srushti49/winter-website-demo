6b189be43c083cee8c5e842238fa8710c9564dbd
Srushti49 <147423565+Srushti49@users.noreply.github.com>
2023-10-28 06:44:59 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 # Setup backend using AWS S3 (Remote State File) and DynamoDB (lock)
 terraform {
   backend "s3" {
-    bucket         = "ctl-tf-state-lock-demo"
+    bucket         = "my-s3-demo-bucket-01"
     key            = "s3"
     region         = "ap-south-1"
-    dynamodb_table = "tf-lock-state"
+    dynamodb_table = "mydbtable"
   }
 }
 
