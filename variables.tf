593d56f3e8a9426566396ddde18033ba8675f9a5
Srushti49 <147423565+Srushti49@users.noreply.github.com>
2023-10-28 06:50:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "static_bucket_name"{
     type = string
-    default = "ctl-static-website-devops"
+    default = "ctl-static-website-devops-01"
 }
 
 variable "logging_bucket_name"{
     type = string
-    default = "logs-ctl-static-website-devops"
+    default = "logs-ctl-static-website-devops-01"
 }
